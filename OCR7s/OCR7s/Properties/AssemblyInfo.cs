9da92e62dd98514d552c70c9f010bc043f321ff7
regulusgc <chupaalperro1718@gmail.com>
2019-04-11 21:28:24 -0600

    todo arriba

---
+++ OCR7s/OCR7s/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("OCR7s")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OCR7s")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita acceder a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en dicho tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("4e1af1ce-a460-4889-b63f-48e0d23577b2")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter "*", como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
